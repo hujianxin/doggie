c36126239c78a1f2aee27d0ad70ee81eede91d98
hujianxin <hujianxin@xiaomi.com>
2018-12-23 16:09:30 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="pybase",
+    version="0.1",
+    author="hujianxin",
+    author_email="hujianxincn@foxmail.com",
+    include_package_data=True,
+    install_requires=[],
+    license="Apache License",
+    packages=["pybase"],
+    description="Python library for interacting to hbase shell",
+)
