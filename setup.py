cd560a13a1e445b779c2b8756b938271e9c84df0
hujianxin <hujianxin@xiaomi.com>
2019-03-22 11:56:17 +0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 
 setup(
     name="doggie",
-    version="0.1",
+    version="0.1.1",
     author="hujianxin",
     author_email="hujianxincn@foxmail.com",
     include_package_data=True,
     install_requires=[],
+    scripts=["doggie.py"],
     license="Apache License",
     description="Python library for interacting to hbase shell",
 )
